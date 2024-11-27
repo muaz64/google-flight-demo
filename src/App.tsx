74ce987d87fa7835aebf8fa3c48e6c0960f6d50e
muaz64 <muazmuhammad21@gmail.com>
2024-11-27 08:38:34 +0600

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-
-
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import hero from "../src/assets/hero.svg";
+import SearchBar from "./components/SearchBar";
 
 function App() {
- 
-
   return (
-    <h1 className="text-3xl font-bold text-center">
-    Hello world!
-  </h1>
-  )
+    <div className="overflow-hidden">
+      <img className="p-14" src={hero} alt="hero-image" />
+      <div className="m-auto my-15">
+        <h1 className="text-center font-semibold text-5xl mt-[-50px] lg:mt-[-150px]">Flights</h1>
+      </div>
+      <SearchBar />
+    </div>
+  );
 }
 
-export default App
+export default App;
